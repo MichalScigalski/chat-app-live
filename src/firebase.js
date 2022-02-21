c9ea7f309ea3c2cf4079bae398b8414cc1e627e9
MichalScigalski <michalscigalski0@gmail.com>
2022-02-22 00:01:31 +0100

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,9 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 
-//replace your config below
+//paste your config below
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyD1k-UwbkgTv80vKmN33aWMHO3Xyde_GuI",
-    authDomain: "reactchatlive.firebaseapp.com",
-    projectId: "reactchatlive",
-    storageBucket: "reactchatlive.appspot.com",
-    messagingSenderId: "661970197463",
-    appId: "1:661970197463:web:ab58446a2910da3c060992"
+
 })
 
 const db = firebaseApp.firestore();
