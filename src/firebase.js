748c65531b39d636112e5953d27521c2fd72da66
MichalScigalski <michalscigalski0@gmail.com>
2022-05-21 18:04:10 +0200

    config added

--- src/firebase.js
+++ src/firebase.js
@@ -3,9 +3,14 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 
-//paste your config below
 const firebaseApp = firebase.initializeApp({
-
+    apiKey: "AIzaSyD3GHCow0aD5On2TBWDT08X3y0PI7AI6lI",
+    authDomain: "chatapp-55976.firebaseapp.com",
+    projectId: "chatapp-55976",
+    storageBucket: "chatapp-55976.appspot.com",
+    messagingSenderId: "682638149321",
+    appId: "1:682638149321:web:772f69883182c3d4b7af18",
+    measurementId: "G-H42L19QZKQ"
 })
 
 const db = firebaseApp.firestore();
